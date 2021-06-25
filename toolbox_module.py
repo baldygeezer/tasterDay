ba7420cd674395ba99d6cfc633a16c3d06c55195
baldygeezer <b.a.roper@soton.ac.uk>
2021-06-25 14:59:52 +0100

    commit

---
+++ toolbox_module.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def drop_values(df: pd.DataFrame, column, value):
+    df.drop(df.index[df[column] == value], inplace=True)
+
+
+def count_values(df: pd.DataFrame,value):
+    for col in df.columns:
+        if df[col].dtype == object:
+            print(col, df[col][df[col] == value].count())
+
+
